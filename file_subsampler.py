fb08e59ad2021d0bc7d58b033b92c4f8dde8f47e
LindaSt <linda.studer@unifr.ch>
2021-03-18 16:20:21 +0100

    Extended the file subsampler (see README.md for new parameters)

--- file_subsampler.py
+++ file_subsampler.py
@@ -5,11 +5,20 @@ import os
 import shutil
 
 
-def file_subsampler(input_path, output_path, file_percentage, random_file_sample=False, symlink=False):
+def _get_samples_files(file_list, file_percentage, random_file_sample):
+    if random_file_sample:
+        samples_files = random.sample(file_list, max(1, int(file_percentage * len(file_list))))
+    else:
+        samples_files = file_list[:max(1, int(file_percentage * len(file_list)))]
+    return samples_files
+
+
+def file_subsampler(input_path, output_path, file_percentage, file_ending=".gxl", random_file_sample=False,
+                    symlink=False, train_only=False):
     output_p = Path(output_path)
     output_p.mkdir(parents=True, exist_ok=True)
 
-    f = [(dirpath, dirnames, filenames) for dirpath, dirnames, filenames in os.walk(input_path)]
+    f = [(dirpath, dirnames, filenames) for dirpath, dirnames, filenames in os.walk(input_path) if len(filenames) > 0 and file_ending in filenames[0]]
     dir_files_dict = {tup[0]: sorted([i for i in tup[2] if i[0] != '.']) for tup in f if len(tup[1]) == 0}
 
     # create the folder structure
@@ -23,10 +32,14 @@ def file_subsampler(input_path, output_path, file_percentage, random_file_sample
         out_folder = in_folder.replace(input_path, output_path)
         Path(out_folder).mkdir(parents=True, exist_ok=True)
         # reduce file list
-        if random_file_sample:
-            samples_files = random.sample(file_list, max(1, int(file_percentage * len(file_list))))
+        if train_only:
+            if 'train' in in_folder:
+                samples_files = _get_samples_files(file_list, file_percentage, random_file_sample)
+            else:
+                samples_files = file_list
         else:
-            samples_files = file_list[:max(1, int(file_percentage * len(file_list)))]
+            samples_files = _get_samples_files(file_list, file_percentage, random_file_sample)
+
         for file in samples_files:
             if symlink:
                 print(f'Creating symlink for {os.path.join(in_folder, file)}')
